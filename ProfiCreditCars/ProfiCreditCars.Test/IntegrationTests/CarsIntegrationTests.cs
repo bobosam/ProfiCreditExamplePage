3c841f3154813b815838c351ecfd7873354c8014
Boyko Andonov <Andonov>
2016-09-22 13:44:16 +0300

    Integration tests commit

---
+++ ProfiCreditCars/ProfiCreditCars.Test/IntegrationTests/CarsIntegrationTests.cs
@@ -0,0 +1,166 @@
+﻿namespace ProfiCreditCars.Test.IntegrationTests
+{
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    using EntityFramework.Extensions;
+    using Microsoft.Owin.Testing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Owin;
+
+    using ProfiCreditCars.Data;
+    using ProfiCreditCars.Services;
+
+    [TestClass]
+    public class CarsIntegrationTests
+    {
+        private static TestServer httpTestServer;
+        private static HttpClient httpClient;
+
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            httpTestServer = TestServer.Create(appBuilder =>
+            {
+                var config = new HttpConfiguration();
+                WebApiConfig.Register(config);
+                appBuilder.UseWebApi(config);
+            });
+
+            httpClient = httpTestServer.HttpClient;
+            var dbContext = new ProfiCreditCarsContext();
+        }
+
+        [TestMethod]
+        public void AddCar_WithValidData_ShouldAddCar()
+        {
+            var context = new ProfiCreditCarsContext();
+
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("model", "BMV"),
+                new KeyValuePair<string, string>("year", "2012"),
+                new KeyValuePair<string, string>("color", "red")
+            });
+
+            var response = this.AddNewCar(data);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void AddCar_WithShortModelName_ShouldReturnBadRequest()
+        {
+            var context = new ProfiCreditCarsContext();
+
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("model", "B"),
+                new KeyValuePair<string, string>("year", "2012"),
+                new KeyValuePair<string, string>("color", "red")
+            });
+
+            var response = this.AddNewCar(data);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void AddCar_WithLongerModelName_ShouldReturnBadRequest()
+        {
+            var context = new ProfiCreditCarsContext();
+
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("model", "Bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"),
+                new KeyValuePair<string, string>("year", "2012"),
+                new KeyValuePair<string, string>("color", "red")
+            });
+
+            var response = this.AddNewCar(data);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+
+        [TestMethod]
+        public void AddCar_WithPreviousYear_ShouldReturnBadRequest()
+        {
+            var context = new ProfiCreditCarsContext();
+
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("model", "Opel"),
+                new KeyValuePair<string, string>("year", "1700"),
+                new KeyValuePair<string, string>("color", "red")
+            });
+
+            var response = this.AddNewCar(data);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void AddCar_WithShortColorString_ShouldReturnBadRequest()
+        {
+            var context = new ProfiCreditCarsContext();
+
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("model", "Honda"),
+                new KeyValuePair<string, string>("year", "2012"),
+                new KeyValuePair<string, string>("color", "r")
+            });
+
+            var response = this.AddNewCar(data);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void AddCar_WithLongerColorString_ShouldReturnBadRequest()
+        {
+            var context = new ProfiCreditCarsContext();
+
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("model", "Honda"),
+                new KeyValuePair<string, string>("year", "2012"),
+                new KeyValuePair<string, string>("color", "Bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+            });
+
+            var response = this.AddNewCar(data);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetAllCars_ShouldReturnOk()
+        {
+            var context = new ProfiCreditCarsContext();
+                       
+            var response = this.GetAllCar();
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestCleanup]
+        private static void CleanDatabase()
+        {
+            var context = new ProfiCreditCarsContext();
+            context.Cars.Delete();
+        }
+
+        private HttpResponseMessage AddNewCar(FormUrlEncodedContent data)
+        {
+            return httpClient.PostAsync("/api/cars", data).Result;
+        }
+
+        private HttpResponseMessage GetAllCar()
+        {
+            return httpClient.GetAsync("/api/cars").Result;
+        }
+    }
+}
---
+++ ProfiCreditCars/ProfiCreditCars.Test/UnitTests/CarsControllerUnitTests.cs
@@ -0,0 +1,112 @@
+﻿namespace ProfiCreditCars.Test.UnitTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Web.Http;
+
+    using ProfiCreditCars.Data;
+    using ProfiCreditCars.Models;
+    using ProfiCreditCars.Services.Controllers;
+    using ProfiCreditCars.Services.Models;
+
+    [TestClass]
+    public class CarsControllerUnitTests
+    {
+        private MockContainer mocks;
+
+        [TestInitialize]
+        public void InitTest()
+        {
+            this.mocks = new MockContainer();
+            this.mocks.PrepareMocks();
+        }
+
+        [TestMethod]
+        public void GetAllCars_ShouldReturnAllCars()
+        {
+            var fakeCars = this.mocks.CarRepositoryMock.Object.All();
+            var mockContext = new Mock<IProfiCreditCarsData>();
+            mockContext.Setup(m => m.Cars.All()).Returns(fakeCars);
+            var carsController = new CarsController(mockContext.Object);
+            this.SetupController(carsController);
+
+            var response = carsController.GetCars().ExecuteAsync(CancellationToken.None).Result;
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void AddCar_WithValithData_ShouldAddNewCar()
+        {
+            var cars = new List<Car>();
+
+            this.mocks.CarRepositoryMock
+                .Setup(c => c.Add(It.IsAny<Car>()))
+                .Callback((Car car) =>
+                {
+                    cars.Add(car);
+                });
+
+            var mockContext = new Mock<IProfiCreditCarsData>();
+            mockContext.Setup(m => m.Cars).Returns(this.mocks.CarRepositoryMock.Object);
+
+            var carsControler = new CarsController(mockContext.Object);
+            this.SetupController(carsControler);
+
+            var randomModel = Guid.NewGuid().ToString();
+            var newCar = new CarBindingModel()
+            {
+                Model = randomModel,
+                Year = 2000,
+                Color = "red"
+            };
+
+            var response = carsControler.PostCar(newCar).ExecuteAsync(CancellationToken.None).Result;
+
+            mockContext.Verify(c => c.SaveChanges(), Times.Once);
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Assert.AreEqual(cars.Count, 1);
+            Assert.AreEqual(newCar.Model, cars[0].Model);
+        }
+
+        [TestMethod]
+        public void DeleteCar_ValidData_SholdDelete()
+        {
+            var fakeCars = this.mocks.CarRepositoryMock.Object.All();
+            var mockContext = new Mock<IProfiCreditCarsData>();
+            mockContext.Setup(m => m.Cars.All()).Returns(fakeCars);
+            var carsController = new CarsController(mockContext.Object);
+            this.SetupController(carsController);
+
+            var response = carsController.DeleteCar(4).ExecuteAsync(CancellationToken.None).Result;
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public void DeleteCar_WrongId_SholdReturnNotFound()
+        {
+            var fakeCars = this.mocks.CarRepositoryMock.Object.All();
+            var mockContext = new Mock<IProfiCreditCarsData>();
+            mockContext.Setup(m => m.Cars.All()).Returns(fakeCars);
+            var carsController = new CarsController(mockContext.Object);
+            this.SetupController(carsController);
+
+            var response = carsController.DeleteCar(1).ExecuteAsync(CancellationToken.None).Result;
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.NotFound);
+        }
+
+        private void SetupController(CarsController carsController)
+        {
+            carsController.Request = new HttpRequestMessage();
+            carsController.Configuration = new HttpConfiguration();
+        }
+    }
+}
