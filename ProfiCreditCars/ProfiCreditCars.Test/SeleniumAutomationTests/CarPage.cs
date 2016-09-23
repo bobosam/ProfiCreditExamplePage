7ab5fc1391af350256c5a671520575e2a4c33456
Boyko Andonov <Andonov>
2016-09-23 08:48:06 +0300

    selenium tests commit

---
+++ ProfiCreditCars/ProfiCreditCars.Test/SeleniumAutomationTests/CarPage.cs
@@ -0,0 +1,31 @@
+﻿namespace ProfiCreditCars.Test.SeleniumAutomationTests
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.PageObjects;
+
+    public class CarPage
+    {
+        private IWebDriver driver;
+
+        public CarPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "model")]
+        public IWebElement Model { get; set; }
+
+        [FindsBy(How = How.Id, Using = "year")]
+        public IWebElement Year { get; set; }
+
+        [FindsBy(How = How.Id, Using = "color")]
+        public IWebElement Color { get; set; }
+
+        [FindsBy(How = How.Id, Using = "add")]
+        public IWebElement AddButton { get; set; }
+
+        [FindsBy(How = How.Id, Using = "pre")]
+        public IWebElement Pre { get; set; }
+    }
+}
---
+++ ProfiCreditCars/ProfiCreditCars.Test/SeleniumAutomationTests/ProfiCreditCarsSeleniumTests.cs
@@ -0,0 +1,166 @@
+﻿namespace ProfiCreditCars.Test.SeleniumAutomationTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+   
+    [TestClass]
+    public class ProfiCreditCarsSeleniumTests
+    {
+        private const string UrlString = "http://localhost:63342/carsjs/index.html";
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.driver = new ChromeDriver();
+
+            this.driver.Navigate().GoToUrl(UrlString);
+            this.driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+            this.driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void AddCar_ValidData_ShouldBeAdd()
+        {
+            CarPage carPage = new CarPage(this.driver);
+
+            carPage.Model.SendKeys("BMV");
+            carPage.Year.SendKeys("2010");
+            carPage.Color.SendKeys("red");
+            carPage.AddButton.Click();
+
+            var message = carPage.Pre.GetAttribute("textContent");
+
+            Assert.AreEqual(message, "success");
+        }
+
+        [TestMethod]
+        public void AddCar_JSModelInjection_ShouldBeAddWithSameModelName()
+        {
+            CarPage carPage = new CarPage(this.driver);
+
+            carPage.Model.SendKeys("<script>alert(1)</script>");
+            carPage.Year.SendKeys("2010");
+            carPage.Color.SendKeys("red");
+            carPage.AddButton.Click();
+
+            var message = carPage.Pre.GetAttribute("textContent");
+
+            Assert.AreEqual(message, "success");
+        }
+
+        [TestMethod]
+        public void AddCar_ShortModelName_ShouldBeErrorMessage()
+        {
+            CarPage carPage = new CarPage(this.driver);
+
+            carPage.Model.SendKeys("B");
+            carPage.Year.SendKeys("2010");
+            carPage.Color.SendKeys("red");
+            carPage.AddButton.Click();
+
+            var message = carPage.Pre.GetAttribute("textContent");
+
+            Assert.AreEqual(message, "error");
+        }
+
+        [TestMethod]
+        public void AddCar_LongerModelName_ShouldBeErrorMessage()
+        {
+            CarPage carPage = new CarPage(this.driver);
+
+            carPage.Model.SendKeys("Bbbbbbbbbbbbbbbbbbbbbbbbbb");
+            carPage.Year.SendKeys("2010");
+            carPage.Color.SendKeys("red");
+            carPage.AddButton.Click();
+
+            var message = carPage.Pre.GetAttribute("textContent");
+
+            Assert.AreEqual(message, "error");
+        }
+
+        [TestMethod]
+        public void AddCar_JSYearlInjection_ShouldBeErrorMessaget()
+        {
+            CarPage carPage = new CarPage(this.driver);
+
+            carPage.Model.SendKeys("Honda");
+            carPage.Year.SendKeys("<script>alert(1)</script>");
+            carPage.Color.SendKeys("red");
+            carPage.AddButton.Click();
+
+            var message = carPage.Pre.GetAttribute("textContent");
+
+            Assert.AreEqual(message, "error");
+        }
+
+        [TestMethod]
+        public void AddCar_EarlierYear_ShouldBeErrorMessaget()
+        {
+            CarPage carPage = new CarPage(this.driver);
+
+            carPage.Model.SendKeys("Honda");
+            carPage.Year.SendKeys("1767");
+            carPage.Color.SendKeys("red");
+            carPage.AddButton.Click();
+
+            var message = carPage.Pre.GetAttribute("textContent");
+
+            Assert.AreEqual(message, "error");
+        }
+
+        [TestMethod]
+        public void AddCar_JSColorInjection_ShouldBeAddWithSameColorName()
+        {
+            CarPage carPage = new CarPage(this.driver);
+
+            carPage.Model.SendKeys("Opel");
+            carPage.Year.SendKeys("2010");
+            carPage.Color.SendKeys("<script>alert(1)</script>");
+            carPage.AddButton.Click();
+
+            var message = carPage.Pre.GetAttribute("textContent");
+
+            Assert.AreEqual(message, "success");
+        }
+
+        [TestMethod]
+        public void AddCar_ShortColor_ShouldBeErrorMessage()
+        {
+            CarPage carPage = new CarPage(this.driver);
+
+            carPage.Model.SendKeys("Opel");
+            carPage.Year.SendKeys("2010");
+            carPage.Color.SendKeys("r");
+            carPage.AddButton.Click();
+
+            var message = carPage.Pre.GetAttribute("textContent");
+
+            Assert.AreEqual(message, "error");
+        }
+
+        [TestMethod]
+        public void AddCar_LongerColor_ShouldBeErrorMessage()
+        {
+            CarPage carPage = new CarPage(this.driver);
+
+            carPage.Model.SendKeys("Opel");
+            carPage.Year.SendKeys("2010");
+            carPage.Color.SendKeys("Bbbbbbbbbbbbbbbbbbbbbbbbbb");
+            carPage.AddButton.Click();
+
+            var message = carPage.Pre.GetAttribute("textContent");
+
+            Assert.AreEqual(message, "error");
+        }
+
+        [TestCleanup]
+        public void Close()
+        {
+            this.driver.Close();
+        }
+    }
+}
